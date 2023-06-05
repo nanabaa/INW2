7e7ecae8be148546aef411363c4c9972da0e4fd6
Logon Aluno <logonrmlocal@fiap.com.br>
2023-06-05 12:19:45 -0300

    revisao

---
+++ exForms/oi.js
@@ -0,0 +1,19 @@
+console.log("hello world\n\nboobs\n")
+console.log("Nome\tNota\tTurma")
+console.log("Chuu\t10\t2")
+console.log("Fredbi\t7\t1")
+
+
+let nome="predo"
+let valor1=10
+let valor2=20
+console.log("Seja bem-vindo"+nome.toLowerCase())
+console.log("a soma dos valores sao: "+(valor1+valor2))
+
+
+const leia = require("prompt-sync")()
+let numero1, numero2 = 0
+numero1 = parseInt(leia("digite o primeiro numero: "))
+numero2 = parseInt(leia("digite o segundo numero: "))
+console.log("a soma de "+numero1+"mais "+numero2+"é "+(numero1+numero2))
+
