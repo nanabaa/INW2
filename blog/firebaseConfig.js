4983d849984999bd79cb8daeaf76edd975995e81
Logon Aluno <logonrmlocal@fiap.com.br>
2023-06-12 12:21:42 -0300

    blog wip

---
+++ blog/firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAGwxi0unkcoMpXLPpeaiWbOwmSC-gRipo",
+    authDomain: "blog-df5fe.firebaseapp.com",
+    projectId: "blog-df5fe",
+    storageBucket: "blog-df5fe.appspot.com",
+    messagingSenderId: "512703178156",
+    appId: "1:512703178156:web:eb9feeebf3c70814c70609"
+  };
+  
+
+const app = initializeApp(firebaseConfig);
